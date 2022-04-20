a6f68ab62afd30dd836780e7c806ec49881edf64
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-04-20 04:14:52 +0100

    done with tracker

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,8 @@ const location = ref(null);
 let myMap;
 
 onMounted(() => {
-  myMap = leaflet.map("map").setView([51.505, -0.09], 13);
+  myMap = leaflet.map("map").setView([6.671741, 3.152513], 13);
+  leaflet.marker([6.671741, 3.152513]).addTo(myMap);
   leaflet
     .tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGFibG9kYWxwaGEiLCJhIjoiY2wxaGN5dGZtMDNuNjNjcWx2ZHJ6bmMzbSJ9.lzihlzjJGb0E_B0viYNQyw",
@@ -68,7 +69,16 @@ const getIpInfo = async () => {
     <div class="flex flex-col h-screen max-h-screen">
       <!-- search / result -->
       <div
-        class="z-20 flex justify-center bg-[hsla(5,87%,31%,0)] relative px-[1rem] pt-8 pb-32"
+        class="
+          z-20
+          flex
+          justify-center
+          bg-[hsla(5,87%,31%,0)]
+          relative
+          px-[1rem]
+          pt-8
+          pb-32
+        "
       >
         <!-- search -->
         <div class="w-full max-w-screen-sm">
@@ -85,7 +95,18 @@ const getIpInfo = async () => {
             />
             <div @click="getIpInfo">
               <i
-                class="fa-solid fa-angles-right cursor-pointer text-white bg-black px-4 py-4 rounded-tr-md flex items-center rounded-br-md"
+                class="
+                  fa-solid fa-angles-right
+                  cursor-pointer
+                  text-white
+                  bg-black
+                  px-4
+                  py-4
+                  rounded-tr-md
+                  flex
+                  items-center
+                  rounded-br-md
+                "
               ></i>
             </div>
           </div>
